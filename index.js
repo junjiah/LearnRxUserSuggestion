cf3ce8b7c8bccc7ef8771e17abbf6bc2bbe91768
EDFward <edfward@gmail.com>
2016-01-08 10:49:39 -0500

    Clear user info before rewriting

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import colors from 'colors';
-import EventEmitter from 'events';
+import { EventEmitter } from 'events';
 import readline from 'readline';
 import rq from 'request-promise';
 import Rx from '@reactivex/rxjs';
-import util from 'util';
 
 // Configure the terminal.
 readline.cursorTo(process.stdout, 0, 0);
@@ -14,22 +13,22 @@ readline.createInterface({
 });
 const userColors = ['cyan', 'red', 'green'];
 const promptString = 'Hit Enter to refresh all Github suggested users\n' +
-    'Or hit key 1 to refresh suggested user 1\n'.cyan +
-    'Or hit key 2 to refresh suggested user 2\n'.red +
-    'Or hit key 3 to refresh suggested user 3\n'.green;
+  'Or hit key 1 to refresh suggested user 1\n'.cyan +
+  'Or hit key 2 to refresh suggested user 2\n'.red +
+  'Or hit key 3 to refresh suggested user 3\n'.green;
 process.stdout.write(promptString);
 
 // Define cursor positions (hard coded for simplicity).
-const startPos = {x: 48, y: 0};
+const startPos = { x: 48, y: 0 };
 const suggestionPos = [
-  {x: 0, y: 6},
-  {x: 0, y: 12},
-  {x: 0, y: 18}
+  { x: 0, y: 6 },
+  { x: 0, y: 12 },
+  { x: 0, y: 18 }
 ];
 
 // Define event emitters.
-const hitEnterEmitter = new EventEmitter();
-const hitNumberEmitters = [...Array(3)].map(() => new EventEmitter());
+const hitEnterEmitter = new EventEmitter();  // For Enter key.
+const hitNumberEmitters = [...Array(3)].map(() => new EventEmitter());  // For key 1, 2, 3.
 
 // Hookup user inputs.
 process.stdin.setRawMode(true);
@@ -51,18 +50,16 @@ process.stdin.on('data', key => {
   }
 });
 
-
 const hitEnterStream = Rx.Observable.fromEvent(hitEnterEmitter, 'hit');
-const hitNumberStreams = hitNumberEmitters.map(emitter =>
-    Rx.Observable.fromEvent(emitter, 'hit')
-);
+const hitNumberStreams = hitNumberEmitters
+  .map(emitter => Rx.Observable.fromEvent(emitter, 'hit'));
 
 const requestStream = hitEnterStream
-    .startWith('startup event')
-    .map(() => {
-      const randomOffset = Math.floor(Math.random() * 500);
-      return `https://api.github.com/users?since=${randomOffset}`;
-    });
+  .startWith('startup event')
+  .map(() => {
+    const randomOffset = Math.floor(Math.random() * 500);
+    return `https://api.github.com/users?since=${randomOffset}`;
+  });
 
 const responseStream = requestStream.flatMap(requestUrl => {
   const options = {
@@ -70,7 +67,7 @@ const responseStream = requestStream.flatMap(requestUrl => {
     headers: {
       'User-Agent': 'Request-Promise',
       // Set access token as an environment variable.
-      'Authorization': `token ${process.env.GITHUB_TOKEN}`
+      'Authorization': `token ${process.env['GITHUB_TOKEN']}`
     },
     json: true
   };
@@ -80,20 +77,20 @@ const responseStream = requestStream.flatMap(requestUrl => {
 
 // Let's have 3 user suggestions, as in the tutorial.
 const suggestionStreams = [...Array(3)].map(
-    (_, i) =>
-        hitNumberStreams[i]
-            .startWith('startup event')
-            .combineLatest(responseStream, (_, arr) =>
-                arr[Math.floor(Math.random() * arr.length)])
-            .merge(hitEnterStream.map(() => null))
-);
+  (_, i) =>
+    hitNumberStreams[i]
+      .startWith('startup event')
+      .combineLatest(responseStream, (_, arr) =>
+        arr[Math.floor(Math.random() * arr.length)])
+      .merge(hitEnterStream.map(() => null))
+  );
 
 // Render Github users in command line.
 function prettify(githubUser) {
   return `Username: ${githubUser['login']}\n` +
-      `ID: ${githubUser['id']}\n` +
-      `Link: ${githubUser['html_url']}\n` +
-      `Admin: ${githubUser['site_admin']}`;
+    `ID: ${githubUser['id']}\n` +
+    `Link: ${githubUser['html_url']}\n` +
+    `Admin: ${githubUser['site_admin']}`;
 }
 
 function cursorTo({x, y}) {
@@ -103,15 +100,15 @@ function cursorTo({x, y}) {
 suggestionStreams.map((stream, i) => {
   stream.subscribe(user => {
     cursorTo(suggestionPos[i]);
-    if (user == null) {
-      // Enter key hit, flush the user info.
-      const infoLineNum = 4;
-      for (let i = 0; i < infoLineNum; i++) {
-        readline.clearLine(process.stdout, 0);
-        readline.moveCursor(process.stdout, 0, 1);
-      }
-    } else {
-      // Print the user info.
+    // Flush the user info at first.
+    const infoLineNum = 4;
+    for (let i = 0; i < infoLineNum; i++) {
+      readline.clearLine(process.stdout, 0);
+      readline.moveCursor(process.stdout, 0, 1);
+    }
+    // Print user info if available.
+    if (user != null) {
+      cursorTo(suggestionPos[i]);
       process.stdout.write(prettify(user)[userColors[i]]);
     }
     cursorTo(startPos);
